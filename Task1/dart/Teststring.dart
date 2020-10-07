658b26ee73c1ca8ddf8d01dd8ac2c59169cc0b8b
AYAN SIDDIQUI <ayaansiddiqui851@gmail.com>
2020-10-06 23:53:30 -0700

    Testing string dart hactober fest code

---
+++ Task1/dart/Teststring.dart
@@ -0,0 +1,13 @@
+void main() {
+   int a = 10, b = 20, c = 5;
+   c = c * c * c;
+
+   print("$a + $b = ${a+b}");
+   print("$a%$b = ${a%b}");
+
+   // Add a break point here
+   
+   print("$a*$b = ${a*b}");
+   print("$a/$b = ${a/b}");
+   print(c);
+}
